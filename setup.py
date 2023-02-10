d7be382c9947677ee163f42596c2a75f41b61fb4
Gghosh55 <ghoshgaurav55@gmail.com>
2023-02-10 20:57:18 +0530

    Updated

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import find_packages, setup
+from typing import List
 
-def get_requirements():
-    pass
+REQUIREMENT_FILE_NAME="requirements.txt"
+HYPEN_E_DOT = "-e"
 
+def get_requirements()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirements_file:
+        requirement_list = requirements_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+
+    if HYPEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPEN_E_DOT)
+    return requirement_list
 setup(
     name="sensor",
     version="0.0.2",
